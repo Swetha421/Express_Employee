68056b75e26a478dbef2a2469b926a603a2d0a2b
918281820301 <swethapsijk2018@gmail.com>
2024-06-25 16:15:11 +0530

    new

--- app.js
+++ app.js
@@ -17,6 +17,23 @@ app.get('/add',(req,res)=>
 {
     res.sendFile(path.join(__dirname,'public','Add.html'))
 })
+app.get('/details',(req,res)=>
+{
+    res.sendFile(path.join(__dirname,'public','employee.html'))
+});
+
+app.post('/adddetail',(req,res)=>{
+  const {ID,empname,role}=req.body;
+  const data={ID,empname,role};
+  employeedetails.push(data)
+  console.log(employeedetails)
+  res.sendFile(path.join(__dirname,'public','employee.html'))
+})
+
+app.get('/alldata',(req,res)=>{
+  res.json(employeedetails);
+  
+})
 
 app.listen(3002, () => {
     console.log('Server is running on port 3002');
